8f785a01c1553d735531a7b3a7d5fa11a6105fad
Benjamin Braniff <78331810+BenBraniff@users.noreply.github.com>
2024-10-04 13:24:42 -0400

    Update Darkmode.js

--- Darkmode.js
+++ Darkmode.js
@@ -1,11 +1,4 @@
-<script>
 function ToggleMode() {
-   if (getItem(DarkMode) == True){
-      setItem(DarkMode, False)
-   }else: {
-      setItem(DarkMode, True)
-   }
    var element = document.Body;
    element.classList.toggle("dark-mode");
 }
-</script>
