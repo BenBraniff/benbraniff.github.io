4de9e893df93e366548d73729619dae9ed9feb4b
Benjamin Braniff <78331810+BenBraniff@users.noreply.github.com>
2024-10-09 00:28:51 -0400

    Update DarkMode.js

--- DarkMode.js
+++ DarkMode.js
@@ -32,17 +32,13 @@ const toggleButton = document.getElementById('toggleDarkMode');
 // Apply system preference on load
 if (prefersDarkScheme.matches) {
     document.body.classList.add('dark-mode');
-} else {
-    document.body.classList.add('light-mode');
 }
 
 // Toggle button to switch between modes
 toggleButton.addEventListener('click', function () {
     if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode');
-        document.body.classList.add('light-mode');
     } else {
-        document.body.classList.remove('light-mode');
         document.body.classList.add('dark-mode');
     }
 });
